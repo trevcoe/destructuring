90d6371f4c7aff180aff0f220a3037ce01a67c63
trevcoe <88930059+trevcoe@users.noreply.github.com>
2021-08-25 21:07:15 -0400

    Add files via upload

---
+++ Destructuring.js
@@ -0,0 +1,33 @@
+// It prints 8, 1846
+
+// It prints 1846, 1659
+
+// Your name is Alejandro and you like purple
+// Your name is Melissa and you like green
+// Your name is undefined and you like green
+
+// maya 
+// Marisa 
+// Chi 
+
+// "Raindrops on roses"
+
+// "whiskers on kittens"
+
+// "Bright copper kettles",
+//   "warm woolen mittens",
+//   "Brown paper packages tied up with strings"
+
+// [10,30,20]
+
+// const obj = {
+//     numbers: {
+//         a: 1,
+//         b: 2
+//     }
+// };
+
+// let {a,b} = obj.numbers
+
+// const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
+
